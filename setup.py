f65fc9a50e4fea43f0afb36ef31a4d846ede53c6
ZhaoLiu <liuzhao1225@gmail.com>
2024-05-22 15:53:37 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xinference',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # list of your dependencies
+    ],
+)
